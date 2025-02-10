fa2da6ba15413da2d8679ebee6290fbd3cef7eb8
Water-Script <lancemitchj00@gmail.com>
2025-02-10 18:30:29 -0500

    Revert to not reset HEAD

--- webhooks/github.php
+++ webhooks/github.php
@@ -74,8 +74,7 @@ function processRequest() {
     }
 
     // We might have to cd up a level...
-    // shell_exec("git pull origin main");
-    shell_exec("git reset --hard HEAD && git pull");
+    shell_exec("git pull origin main");
     http_response_code(200);
 }
 
