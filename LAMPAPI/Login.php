de9074d90d63361d4458ffa8f03577091b55bba8
Michael Rivera <levelcoolio767@gmail.com>
2025-02-15 23:48:21 -0500

    Added error checking to rest of backend

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,8 +1,13 @@
 <?php
+
+    error_reporting(-1); 
+    ini_set("display_errors", "1"); 
+    ini_set("log_errors", 1); // Report errors in console if it occurs
+
     require 'DBConnection.php';
-    
+
     $inData = getRequestInfo();
-    
+
     function getRequestInfo() {
         return json_decode(file_get_contents('php://input'), true);
     }
