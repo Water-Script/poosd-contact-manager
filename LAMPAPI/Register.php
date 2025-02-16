0b8fcca1763f187762f681556e45d5b323d0ac52
Water-Script <lancemitchj00@gmail.com>
2025-02-16 13:27:33 -0500

    Clean up code
    Clean up code, un-nest nested if statements, and improve the error function as well as improving some of the error messages.

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(-1);
 ini_set("display_errors", "1");
 ini_set("log_errors", 1); // Report errors in console if it occurs
@@ -13,53 +12,58 @@ $password = $inData["password"];
 $checkUser = $conn->prepare("SELECT ID FROM Users WHERE Username=?");
 $checkUser->bind_param("s", $username);
 $checkUser->execute();
-$result = $checkUser->get_result(); // Check if a user exists
+$result = $checkUser->get_result();  // Check if a user exists
 
 if ($row = $result->fetch_assoc()) {
-    returnWithError("A user already exists with this username.");
+    returnWithError("ExistingUserError", "A user already exists with this username.");
     exit();
 }
+
 if (empty($username) || empty($password)) {
-    returnWithError("All fields must be filled.");
-} // Validate fields not being empty
-else {
-    $registerUser = $conn->prepare("INSERT INTO Users (Username,Password) VALUES (?,?)");
-    $registerUser->bind_param("ss", $username, $password);
+    returnWithError("MalformedRequestError", "All fields must be filled.");
+    exit();
+}  // Validate fields not being empty
 
-    if ($registerUser->execute()) {
-        $getRegisterUser = $conn->prepare("SELECT ID FROM Users WHERE Username=?");
-        $getRegisterUser->bind_param("s", $username);
-        $getRegisterUser->execute();
-        $result = $getRegisterUser->get_result(); // create user
+$registerUser = $conn->prepare("INSERT INTO Users (Username,Password) VALUES (?,?)");
+$registerUser->bind_param("ss", $username, $password);
 
-        if ($row = $result->fetch_assoc()) {
-            returnWithInfo($username, $row["ID"]);
-        }
-        else {
-            returnWithError("You should never see this");
-        }
+if ($registerUser->execute()) {
+    $getRegisterUser = $conn->prepare("SELECT ID FROM Users WHERE Username=?");
+    $getRegisterUser->bind_param("s", $username);
+    $getRegisterUser->execute();
+    $result = $getRegisterUser->get_result();  // create user
+
+    if ($row = $result->fetch_assoc()) {
+        returnWithInfo($username, $row["ID"]);
+    } else {
+        header("Content-type: application/json; charset=utf-8");
+        http_response_code(400);
+        echo json_encode(array(
+            "error" => "InternalServerError",
+            "message" => "The server has encountered an internal error during the processing of the request."
+        ));
     }
-
-    $registerUser->close();
-    $getRegisterUser->close();
-    $conn->close();
-    } // create user
 }
+
+$registerUser->close();
+$getRegisterUser->close();
+$conn->close();
 
 function getRequestInfo() {
     return json_decode(file_get_contents("php://input"), true);
 }
 
-function returnWithError ($err) {
+function returnWithError($err, $message) {
     $returnArray = array(
-        "error" => $err
+        "error" => $err,
+        "message" => $message
     );
     header("Content-type: application/json; charset=utf-8");
     http_response_code(400);
     echo json_encode($returnArray);
 }
 
-function returnWithInfo ($username, $id) {
+function returnWithInfo($username, $id) {
     $returnArray = array(
         "userId" => $id,
         "username" => $username
