82343f3b26276c7e7de48d1f775dd790b41ac9be
Michael Rivera <levelcoolio767@gmail.com>
2025-02-15 22:57:26 -0500

    Added error checking

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,5 +1,9 @@
 <?php
 
+    error_reporting(-1); // reports all errors
+    ini_set("display_errors", "1"); // shows all errors
+    ini_set("log_errors", 1); 	
+
     require 'DBConnection.php';
     $inData = getRequestInfo();
 
