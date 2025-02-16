2d2bd888581dc7f409f57295701379430c4aa446
Michael Rivera <levelcoolio767@gmail.com>
2025-02-15 21:53:00 -0500

    Basic registration added

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,9 +1,58 @@
 <?php
-require 'DBConnection.php';
 
-$inData = getRequestInfo();
+    require 'DBConnection.php';
+    $inData = getRequestInfo();
 
-function getRequestInfo() {
-    return json_decode(file_get_contents('php://input'), true);
-}
+    $id = 0;
+    $username = "";
+    $password = ""; // this will be used for checking if an account exists
+
+    // check account exists
+
+    $newUsername = $indata['username'];
+    $newPassword = $indata['password']; 
+
+    if (empty($newUsername) || empty($newPassword)) {
+        returnWithError("All fields must be filled.");
+    } // Validate fields
+    else {
+        $newHashedPassword = password_hash($indata['password'], PASSWORD_BCRYPT); // hashing for security
+        $registerUser = $conn->prepare("INSERT INTO Users (Username,Password) VALUES (?,?)");
+        $registerUser->bind_param("ss", $newUsername, $newHashedPassword);
+        $registerUser->execute();
+        $result = $registerUser->get_result();
+
+        if ($row = $result->fetch_assoc())
+        {
+            returnWithInfo($row['username'], $row['password'], $row['ID']);
+        }
+        else {
+            returnWithError("You should never see this");
+        }
+
+        $registerUser->close();
+        $conn->close();
+    } // create user
+
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj) {
+    	header('Content-type: application/json');
+    	echo $obj;
+    }
+
+    function returnWithError ($err) {
+        $retValue = '{"id":0,"username":"","error":"' . $err . '"}';
+        http_response_code(400);
+        sendResultInfoAsJson($retValue);
+    }
+
+    function returnWithInfo ($username, $id) {
+        $retValue = '{"id":' . $id . ',"username":"' . $username . '","error":""}';
+        http_response_code(201);
+        sendResultInfoAsJson($retValue);
+    }
+
 ?>
