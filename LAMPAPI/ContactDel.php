884b5d8417d8ba4ef989a394f17ce0cad70ad11d
Michael Rivera <levelcoolio767@gmail.com>
2025-02-24 14:57:10 -0500

    Cleaned up return

--- LAMPAPI/ContactDel.php
+++ LAMPAPI/ContactDel.php
@@ -23,7 +23,7 @@ $stmt->bind_param("ii", $contactId, $userId);
 
 if ($stmt->execute()) {
     if ($stmt->affected_rows > 0) {
-        returnWithInfo("ContactDeletedSuccess", "The contact has been deleted successfully.");
+        returnWithInfo();
     } else {
         returnWithError("ContactNotFoundError", "The contact does not exist or does not belong to the user.", 404);
     }
@@ -46,11 +46,8 @@ function returnWithError($err, $message, $code) {
     echo json_encode($returnArray);
 }
 
-function returnWithInfo($success, $message) {
-    $returnArray = array("success" => $success, "message" => $message);
-    header("Content-type: application/json; charset=utf-8");
-    http_response_code(200);
-    echo json_encode($returnArray);
+function returnWithInfo() {
+    http_response_code(204);
 }
 
 ?>
