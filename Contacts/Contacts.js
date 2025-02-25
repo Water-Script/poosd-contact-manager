a4abd2df191b36bc9cd8044b8c3ad7a694d4d9b1
Robert Marrero <ro094649@ucf.edu>
2025-02-24 22:55:56 -0500

    edit and delete buttons

--- Contacts/Contacts.js
+++ Contacts/Contacts.js
@@ -302,9 +302,10 @@ function editToggle(button) {
 
   // Change the Edit button to a Save button (SVG icon)
   button.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-      <path d="M12.146.854a1 1 0 0 1 1.415 1.415L6.62 8.379a6.016 6.016 0 0 0-1.163.685l-1.56.78a.5.5 0 0 0-.268.315l-.493 2.05a.5.5 0 0 0 .149.523l1.155 1.155a.5.5 0 0 0 .523.149l2.05-.493a.5.5 0 0 0 .315-.268l.78-1.56a6.016 6.016 0 0 0 .685-1.163l5.11-6.941a1 1 0 0 1 1.415 1.415l-5.11 6.941a7.978 7.978 0 0 1-.93 1.549l-.77 1.537a.5.5 0 0 0 .122.565l1.5 1.5a.5.5 0 0 0 .707 0l1.5-1.5a.5.5 0 0 0 .122-.565l-.77-1.537a7.978 7.978 0 0 1-.93-1.549l-5.11-6.941A1 1 0 0 1 12.146.854z"/>
-    </svg>`;
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+</svg>`;
   button.setAttribute("onclick", "saveContact(this)");
   button.classList.remove("btn-warning");
   button.classList.add("btn-success");
