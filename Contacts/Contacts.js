9dcc552834de791d08e29681b57879ea677e3b93
Robert Marrero <ro094649@ucf.edu>
2025-02-24 22:37:41 -0500

    edit and delete buttons

--- Contacts/Contacts.js
+++ Contacts/Contacts.js
@@ -202,7 +202,7 @@ function updateTable(contacts) {
             <td>${contact.PhoneNumber}</td>
             <td>${contact.Email}</td>
             <td>
-                <button class="btn btn-warning mr-1" onclick="editContact()">
+                <button class="btn btn-warning mr-1" onclick="editToggle(this)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-pencil-square" viewBox="0 0 16 16">
   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
@@ -290,4 +290,24 @@ function changeInputType() {
     default:
       break;
   }
+}
+
+function editToggle(button) {
+  let contact = button.closest("tr");
+
+  contact.cells[0].contentEditable = true;
+  contact.cells[1].contentEditable = true;
+  contact.cells[2].contentEditable = true;
+  contact.cells[3].contentEditable = true;
+
+  // Change the Edit button to a Save button (SVG icon)
+  let svg = button.querySelector("svg");
+  svg.setAttribute("class", "bi bi-check"); // Change to checkmark icon
+  button.setAttribute("onclick", "saveContact(this)");
+  button.classList.remove("btn-warning");
+  button.classList.add("btn-success");
+}
+
+function saveContact(button) {
+  let contact = button.closest("tr");
 }
