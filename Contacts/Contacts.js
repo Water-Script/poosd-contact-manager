64ce605908163f629d661cc7622d68158ff113bb
Robert Marrero <ro094649@ucf.edu>
2025-02-25 00:24:37 -0500

    Phone number structure added

--- Contacts/Contacts.js
+++ Contacts/Contacts.js
@@ -303,11 +303,6 @@ function editToggle(button) {
   contact.cells[2].contentEditable = true;
   contact.cells[3].contentEditable = true;
 
-  let phone = contact.cells[2].innertext;
-  phone.addEventListener("input", function () {
-    formatPhoneNumber(phone);
-  });
-
   button.innerHTML = `
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
